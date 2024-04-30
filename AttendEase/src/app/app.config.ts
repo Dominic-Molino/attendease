01641e1585bce8241b6c0c78c03011c97d2d377e
月亮 <dommolino28@gmail.com>
2024-04-30 13:11:45 +0800

    custom

--- AttendEase/src/app/app.config.ts
+++ AttendEase/src/app/app.config.ts
@@ -2,16 +2,15 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideNativeDateAdapter } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    // importProvidersFrom(HttpClientModule),
     provideHttpClient(),
-    provideAnimationsAsync(),
     provideNativeDateAdapter(),
+    provideAnimationsAsync(),
   ],
 };
